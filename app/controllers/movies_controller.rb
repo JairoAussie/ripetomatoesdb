9727779ee75600af9c1c1759182602153d6ff435
jairoaussie <jairo.bilbao@coderacademy.edu.au>
2022-02-02 12:04:49 +1000

    new movie form

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,4 +5,16 @@ class MoviesController < ApplicationController
     def show
         @movie = Movie.find(params[:id])
     end
+    def new
+        @movie = Movie.new
+    end
+    def create
+        movie = Movie.create(title: movie_params[:title], genre: movie_params[:genre], year: movie_params[:year].to_i, length: movie_params[:length].to_i)
+
+		redirect_to movie_path(movie.id)
+    end
+
+    def movie_params
+		params.require(:movie).permit(:title, :genre, :year, :length)
+	end
 end
