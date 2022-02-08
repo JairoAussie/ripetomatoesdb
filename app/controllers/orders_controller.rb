3e743c89329a0201e10a452dec5eaa3893d92c35
jairoaussie <jairo.bilbao@coderacademy.edu.au>
2022-02-08 11:37:06 +1000

    stripe

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,8 +5,27 @@ class OrdersController < ApplicationController
         @order = Order.new
     end
     def create
-        Order.create( movie: @movie, user: current_user, price: @movie.price)
-        redirect_to movie_path(@movie.id)
+        @amount = (@movie.price*100).to_i
+
+        customer = Stripe::Customer.create(
+            email: params[:stripeEmail],
+            source: params[:stripeToken]
+        )
+
+        charge = Stripe::Charge.create(
+            customer: customer.id,
+            amount: @amount,
+            description: 'Rails Stripe customer',
+            currency: 'aud'
+        )
+
+        
+        @order = Order.create( movie: @movie, user: current_user, price: @movie.price)
+        #
+        rescue Stripe::CardError => e
+            flash[:error] = e.message
+            redirect_to movie_path(@movie.id)
+            # redirect_to new_charge_path
     end
 
     def find_movie
