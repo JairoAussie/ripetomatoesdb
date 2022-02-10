8b1ec6025320f6568019a21c29d4accfcfb65f2c
jairoaussie <jairo.bilbao@coderacademy.edu.au>
2022-02-10 12:54:14 +1000

    stripe

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,11 @@ Rails.application.routes.draw do
   # get '/movies/:id', to: 'movies#show', as: 'movie'
   # post '/movies', to: 'movies#create'
   root to: "movies#index"
+
   resources :movies, only:[:index, :show, :new, :create] do
     resources :reviews, only:[:new, :create]
     resources :orders, only:[:new, :create]
   end
-  
+  get '/myorders', to: 'orders#index', as: "my_orders"
   get '/actors', to: 'actors#index'
 end
